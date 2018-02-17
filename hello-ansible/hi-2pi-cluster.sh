5c7526cb8db3cd43813e23474628715f1146f129
Mark Bank <mbank@kent.edu>
2018-02-17 15:45:47 -0500

    baby steps -- add groups in inventory

--- hello-ansible/hi-2pi-cluster.sh
+++ hello-ansible/hi-2pi-cluster.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "pinging all... note: -m is a 'module' option, not 'message' ..."
 ansible -u pi --private-key=~/.ssh/pi_id_rsa all -m ping
+
+echo
+echo "doin something for the datanodes group"
+ansible -u pi --private-key=~/.ssh/pi_id_rsa datanodes -a "/bin/echo hey there, datanode"
